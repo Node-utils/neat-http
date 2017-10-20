bb2a1e12f512d5e4595e2eedb8590f0aab8e86cc
zhuqiuyang <zqyhit@qq.com>
2017-10-20 17:19:25 +0800

    add eslint

--- examples/Lbc.js
+++ examples/Lbc.js
@@ -1,37 +1,43 @@
 const neat_http = require('../index.js');
 
-const arr = [{
+const arr = [
+  {
     host: 'localhost',
     port: 8000
   },
   {
     host: 'localhost',
     port: 9000
-  },
+  }
 ];
 
-const client = new neat_http.LBClient({
-  path: '/path'
-}, {
-  rr: arr,
-  timeout: 5000,
-  healthCheckOpts: {
-    path: '/check',
+const client = new neat_http.LBClient(
+  {
+    path: '/path'
   },
-  healthCheckFn: cres => cres.statusCode == 200,
-  healthCheckCycle: 3000, //ms, every cycle will check all hosts
-});
+  {
+    rr: arr,
+    timeout: 5000,
+    healthCheckOpts: {
+      path: '/check'
+    },
+    healthCheckFn: cres => cres.statusCode == 200,
+    healthCheckCycle: 3000 // ms, every cycle will check all hosts
+  }
+);
 
 (async() => {
   for (let i = 0; i < 10; i++) {
     console.log(await client.healthCheck());
   }
-  console.log('done')
+  console.log('done');
   setInterval(() => {
-    client.send({
-      path: '/test/client'
-    }).catch(err => {
-      console.error(err)
-    })
-  }, 500)
-})()
+    client
+      .send({
+        path: '/test/client'
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, 500);
+})();
--- examples/rr.js
+++ examples/rr.js
@@ -1,26 +1,28 @@
-const request = require('../index.js')
+const request = require('../index.js');
 
-const arr = [{
+const arr = [
+  {
     host: 'localhost',
     port: 8000
   },
   {
     host: 'localhost',
     port: 9000
-  },
+  }
 ];
 
-const request = request.createClient({
-  path: '/path'
-}, {
-  rr: arr
-});
+const request = request.createClient(
+  {
+    path: '/path'
+  },
+  {
+    rr: arr
+  }
+);
 
 (async() => {
   for (let i = 0; i < 100; i++) {
     await request();
   }
-  console.log('done')
-})()
-
-
+  console.log('done');
+})();
